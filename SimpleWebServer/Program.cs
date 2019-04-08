607a9e8f8ae352245543a9c68c034ee36a230480
Fredrik Haglund <mejl@fredrikhaglund.se>
2019-04-08 14:31:18 +0200

    Solution!

--- SimpleWebServer/Program.cs
+++ SimpleWebServer/Program.cs
@@ -21,36 +21,78 @@ namespace SimpleWebServer
 
             Console.WriteLine("Waiting for a connection...");
 
-            Socket handler = listener.Accept();
-
-            var ns = new NetworkStream(handler);
-            var reader = new StreamReader(ns);
-            var writer = new StreamWriter(ns);
-
-            var command = reader.ReadLine();
-            Console.WriteLine($"Command {command}");
-
-            var headers = new List<string>();
-
-            var header = reader.ReadLine();
-            while (!string.IsNullOrEmpty(header))
+            while (true)
             {
-                headers.Add(header);
-                Console.WriteLine($"Header _> {header}");
-                header = reader.ReadLine();
-            }
 
-            Console.WriteLine("Got command and headers in REQUEST!");
+                Socket handler = listener.Accept();
 
-            Console.WriteLine("Writing RESPONSE!");
-            writer.WriteLine("HTTP/1.1 200 OK");
-            writer.WriteLine("");
-            writer.WriteLine("<h1>Hello World!</h1>");
-            writer.WriteLine("<p>CMS18 is Awesome!</p>");
-            writer.Flush();
+                var ns = new NetworkStream(handler);
+                var reader = new StreamReader(ns);
+                var writer = new StreamWriter(ns);
 
-            handler.Shutdown(SocketShutdown.Both);
-            handler.Close();
+                var command = reader.ReadLine();
+                var parts = command.Split(' ');
+                var verb = parts[0];
+                var path = parts[1];
+                Console.WriteLine($"Command {command}");
+
+                var headers = new List<string>();
+
+                var header = reader.ReadLine();
+                while (!string.IsNullOrEmpty(header))
+                {
+                    headers.Add(header);
+                    Console.WriteLine($"Header _> {header}");
+                    header = reader.ReadLine();
+                }
+
+                Console.WriteLine("Got command and headers in REQUEST!");
+
+                Console.WriteLine("Writing RESPONSE!");
+
+                if (path.StartsWith("/home"))
+                {
+                    writer.WriteLine("HTTP/1.1 200 OK");
+                    writer.WriteLine("Content-Type: text/html; charset=UTF-8");
+                    writer.WriteLine("Connection: close");
+                    writer.WriteLine("");
+                    writer.WriteLine("<h1>Du är hemma</h1>");
+                    writer.WriteLine("<p>CMS18 is Awesome!</p>");
+                }
+                else if (path.StartsWith("/sayhello"))
+                {
+                    var index = path.IndexOf("name=", StringComparison.InvariantCultureIgnoreCase);
+                    var name = "";
+                    if (index > 0)
+                    {
+                        name = path.Substring(index + 5);
+                    }
+
+                    writer.WriteLine("HTTP/1.1 200 OK");
+                    writer.WriteLine("Content-Type: text/html; charset=UTF-8");
+                    writer.WriteLine("Connection: close");
+                    writer.WriteLine("");
+                    writer.WriteLine($"<h1>Hej {name}!</h1>");
+                }
+                else 
+                {
+                    writer.WriteLine("HTTP/1.1 404 Not Found");
+                    writer.WriteLine("Content-Type: text/html; charset=UTF-8");
+                    writer.WriteLine("Connection: close");
+                    writer.WriteLine("");
+                    writer.WriteLine("<h1>404 Finns i sjön</h1>");
+                }
+
+
+                writer.Flush();
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+
+                Console.WriteLine("Done!");
+            };
+
+
         }
     }
 }
